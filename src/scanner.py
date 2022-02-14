837746c44e0874c8988596b15c745e1f4a69c39a
Sakhawat <saimon.sust@gmail.com>
2022-02-14 17:09:31 -0600

    Working scanner with stdout matcing requirements

--- src/scanner.py
+++ src/scanner.py
@@ -48,105 +48,92 @@ KEYWORDS = {'void' : 'T_Void',
             'false': 'T_BoolConstant'
             }
 
-OP_BINARY = {'||': 'T_Or',
-             '&&': 'T_And', # missing from the document, adding anyway
-             '<=': 'T_LessEqual',
-             '>=': 'T_GreaterEqual',
-             '==': 'T_Equal'
-             }
-
-OP_UNARY = '+-*/<>=;,!{}()'
 R_DELIM = '\s+'
-R_MCOMMENT = '/\*(.|\s)*?\*/'
+R_MCOMMENT = '/\*(.|\s)*?((\*/)|$)'
 R_SCOMMENT = '//[^\n]*\n'
+R_IGNORE = '|'.join([R_DELIM, R_MCOMMENT, R_SCOMMENT])
 
-R_BADSTRING = '"[^\n"]*\n'
+R_BADSTRING = '"[^"\n]*\n'
 
 def count_lines(token):
     return token.count('\n')
 
+def get_val(token_type, token):
+    if token_type in ['T_BoolConstant', 'T_StringConstant']:
+        return token
+    elif token_type == 'T_IntConstant':
+        return int(token)
+    elif token_type == 'T_DoubleConstant':
+        val = float(token)
+        if val - int(val) == 0:
+            val = int(val)
+        return val
+    return None
+
 def announce_token(token, line, start, end, token_type, attr_str = None, msg = None):
-    print('{0:<12} line {1} cols {2}-{3} is {4}{5}{6}'.format(
-        token,
-        line,
-        start,
-        end,
-        token_type,
-        ' (value = {0})'.format(attr_str) if attr_str != None else '',
-        ' ({0})'.format(msg) if msg != None else ''))
+    print('{0:<12} line {1} cols {2}-{3} is {4} {5}{6}'.format(
+        token, line, start, end, token_type,
+        '(value = {0})'.format(attr_str) if attr_str != None else '',
+        '({0})'.format(msg) if msg != None else ''))
     
 def announce_error(msg, line):
-    print('\n*** Error line {0}.'.format(line))
-    print('*** ' + msg + '\n')
+    print('\n*** Error line {0}.\n*** {1}\n'.format(line, msg))
 
 def scan(txt):
-    line = 1
+    line, col = 1, 1
     while txt != '':
-        skipped = False
-        for R in [R_MCOMMENT, R_SCOMMENT, R_DELIM]:
-            if re.match(R, txt):
-                skiptoken = re.match(R, txt).group(0)
-                line += count_lines(skiptoken)
-                txt = txt[len(skiptoken):]
-                if R in [R_MCOMMENT]:
-                    #print('skipped #{0}#'.format(skiptoken))
-                    #print('Remaining:\n', txt[:10])
-                    pass
-                skipped = True
-        if skipped:
-            continue
+        if re.match(R_IGNORE, txt):
+            token = re.match(R_IGNORE, txt).group(0)
+            n_lines = count_lines(token)
+            line += n_lines
+            col = col + len(token) if n_lines == 0 else len(token.split('\n')[-1])+1
+            txt = txt[len(token):]
+        elif re.match(R_BADSTRING, txt): 
+            token = re.match(R_BADSTRING, txt).group(0)
+            announce_error('Unterminated string constant: {0}'.format(token[:-1]), line)
+            line += 1
+            col = 1
+            txt = txt[len(token):]
             
-        for token_type, R_TOKEN in R_TOKENS:
-            if re.match(R_TOKEN, txt):
-                match = re.match(R_TOKEN, txt)
-                token, start, end = match.group(0), match.start()+1, match.end()
-                line += count_lines(token)
-                txt = txt[len(token):]
-                val = None
-                msg = None
-                # Token-type specific actions:
-                if token_type == 'T_Identifier':
-                    if token in KEYWORDS:
-                        token_type = KEYWORDS[token]
-                        if token_type == 'T_BoolConstant':
-                            val = token
-                        announce_token(token, line, start, end, token_type, val, msg = msg)
-                    else:
-                        if len(token) > 31:
-                            announce_error('Identifier too long: "{0}"'.format(token), line)
-                            msg = 'truncated to {0}'.format(token[:31])
-                            announce_token(token, line, start, end, token_type, val, msg = msg)
-                            token = token[:31]
+        else:
+            for token_type, R_TOKEN in R_TOKENS:
+                if re.match(R_TOKEN, txt):
+                    token = re.match(R_TOKEN, txt).group(0)
+                    start, end = col, col + len(token)-1
+                    col = end + 1
+                    val, msg = None, None
+                    txt = txt[len(token):]
+                    if token_type == 'T_Identifier':
+                        if token in KEYWORDS:
+                            token_type = KEYWORDS[token]
+                            if token_type == 'T_BoolConstant':
+                                val = token
+                            announce_token(token, line, start, end, token_type, val, msg)
                         else:
-                            announce_token(token, line, start, end, token_type, val, msg = msg)
-                elif token_type == 'T_IntConstant':
-                    val = int(token)
-                    announce_token(token, line, start, end, token_type, val, msg = msg)
-                elif token_type == 'T_DoubleConstant':
-                    val = float(token)
-                    if val - int(val) == 0:
-                        val = int(val)
-                    token_type += ' (value = {0})'.format(val)
-                    announce_token(token, line, start, end, token_type, val, msg = msg)
-                elif token_type == 'T_StringConstant':
-                    val = token
-                    announce_token(token, line, start, end, token_type, val, msg = msg)
-                else:
-                    announce_token(token, line, start, end, token_type, val, msg = msg)
-                
-                break
-        else:       
-            break
-            
+                            if len(token) > 31:
+                                announce_error('Identifier too long: "{0}"'.format(token), line)
+                                msg = 'truncated to {0}'.format(token[:31])
+                                announce_token(token, line, start, end, token_type, val, msg = msg)
+                                token = token[:31]
+                            else:
+                                announce_token(token, line, start, end, token_type, val, msg = msg)
+                    else:
+                        val = get_val(token_type, token)
+                        announce_token(token, line, start, end, token_type, val, msg = msg)
+                    break
+            else:
+                token = txt[0]
+                announce_error("Unrecognized char: '{0}'".format(token), line)
+                txt = txt[1:]
+                    
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print('Please enter a filepath as the program argument')
     else:
         path = sys.argv[1]
         try:
-            file = open(path)
-            txt = file.read()
-            scan(txt)
-            file.close()
+            with open(path) as file:
+                scan(file.read())
         except IOError:
             print('Could not open file: {0}'.format(path))
