53926812ec563f43bb567fe1df1a801514f9bd8b
Sakhawat <saimon.sust@gmail.com>
2022-03-09 16:30:24 -0600

    Added empty parser module

---
+++ src/parser.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  9 16:22:08 2022
+
+@author: shsaimon
+"""
+
+from scanner import Scanner
+
+class Parser:
+    
+    class Token:
+        pass
